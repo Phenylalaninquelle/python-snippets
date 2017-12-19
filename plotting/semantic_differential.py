46cae41508b5a0ec0cf8deca40bd78bfea8eaa0c
Jan Kiene <jankiene@onlinehome.de>
2017-12-19 11:08:52 +0100

    adjustments (too many)

--- plotting/semantic_differential.py
+++ plotting/semantic_differential.py
@@ -3,16 +3,17 @@ Create a profile plot aka semantic differential
 """
 
 import matplotlib.pyplot as plt
-from numpy import where, isnan, split, arange, asarray
+import numpy as np
 from pandas import DataFrame
 
-def plot_sem_diff(df, x_labels, y_labels, colours, labels, title=''):
+def plot_sem_diff(data, x_labels, y_labels, colours, labels, title=''):
     #TODO: make labels optional or something
     """
-    Plot the semantic differential of the values given by `df`
+    Plot the semantic differential of the values given by `data`
 
-    TODO: df should accept any seuence type
-    df - pandas DataFrame with the data
+    data - sequence containing the data
+        must be either one- or twodimensional
+        with the different observations in the rows and the attributes in the columns
     x_labels - labels for the values on the x axis
     y_labels - sequence of labels for the y axes
         If given a one-dimensional sequence (i.e. a seq of labels), labels will 
@@ -22,20 +23,18 @@ def plot_sem_diff(df, x_labels, y_labels, colours, labels, title=''):
     labels - sequence of strings to use as labels for the lines
     title - title for the figure
     """
-    # error checking
-    if type(df) != DataFrame:
-        raise ValueError("Must pass DataFrame!!")
-    L = len(df)
-    if L != len(colours):
+    data, d_rows, d_cols = _handle_input_data(data)
+    # TODO: handle the colours in a better way
+    if d_rows != len(colours):
         raise ValueError("There must be a colour for every row in the DataFrame!")
 
-    left_labels, right_labels = __get_labels(y_labels) 
+    left_labels, right_labels = _get_labels(y_labels) 
 
     fig = plt.figure()
-    y = arange(len(df.columns))[::-1]
+    y = np.arange(d_cols)[::-1]
 
-    for i in range(L):
-        __do_plot(df.loc[i], y, colour=colours[i], label=labels[i])
+    for i in range(len(data)):
+        _do_plot(data[i], y, colour=colours[i], label=labels[i])
 
     plt.title(title)
     #TODO: this only works for the x labels being numbers
@@ -56,7 +55,23 @@ def plot_sem_diff(df, x_labels, y_labels, colours, labels, title=''):
     plt.legend()
     plt.tight_layout()
 
-def __do_plot(data, y, colour, label):
+    return fig
+
+def _handle_input_data(data):
+    """Helper function for input data validation and calculating helper values"""
+    data = np.asarray(data)
+    if np.ndim(data) == 0:
+        d_rows = 1
+        d_cols = len(data)
+        data = data.reshape((1, data.shape[0]))
+    elif np.ndim(data) == 2:
+        d_rows = data.shape[0]
+        d_cols = data.shape[1]
+    else:
+        raise ValueError("Incorrect dimensionality of data. Must be <= 2")
+    return data, d_rows, d_cols
+
+def _do_plot(data, y, colour, label):
     """
     Wrapper for the actual plotting that takes care of missing values in the data
     (represented by `Nan`).
@@ -66,21 +81,21 @@ def __do_plot(data, y, colour, label):
     colour - colour for the plot
     label - label for the line
     """
-    data = asarray(data)
-    y = asarray(y)
-    if len(data.shape) !=1 and data.shape[1] > 1:
-        raise ValueError("__do_plot handles only one-d data!")
+    data = np.asarray(data)
+    y = np.asarray(y)
+    if np.ndim(data) > 1:
+        raise ValueError("_do_plot handles only one-d data!")
 
-    x, y = __split_by_nan(data, y)
+    x, y = _split_by_nan(data, y)
 
     for x_arr, y_arr in zip(x, y):
         plt.plot(x_arr, y_arr, color=colour, linestyle='--', marker='x', label=label)
 
-def __get_labels(y_labels):
+def _get_labels(y_labels):
     """
     Handle the given y-axis labels.
     """
-    y_labels = asarray(y_labels)
+    y_labels = np.asarray(y_labels)
     dim = y_labels.ndim
     if dim == 1:
         if len(y_labels) == 0:
@@ -95,7 +110,7 @@ def __get_labels(y_labels):
         # sequence of pairs (which makes shape[1] = 2), one could use len(y) to
         # find the axis with the labels and infer the array's shape but if only two
         # labels are given this results in ambiguity (maybe there is another smart
-        # way but for now the arguments has to be in shape (N,2)
+        # way but for now the arguments has to be in shape (N,2))
         if shape[1] != 2:
             raise ValueError("y_labels given in incorrect shape")
         left_labels = y_labels.T[0]
@@ -103,7 +118,7 @@ def __get_labels(y_labels):
     return left_labels, right_labels
 
 
-def __split_by_nan(x, y):
+def _split_by_nan(x, y):
     """
     Split `x` up into subsequences seperated by Nan values. If no Nan values are
     present in `x` then only one subsequence is generated (being `x`). Split up
@@ -112,12 +127,12 @@ def __split_by_nan(x, y):
     Returns two sequences. The first contains the subsequences of `x`, the second
     those of `y`.
     """
-    nan_pos = where(isnan(x))[0]
+    nan_pos = np.where(np.isnan(x))[0]
     if len(nan_pos) == 0:
         return [x], [y]
    
     # split the arrays by nan values in it and then collects the subarrays
     # in a list without the nan value or the corresponding y value respectively
-    x_ret = [a if i==0 else a[1:] for i, a in enumerate(split(x, nan_pos))]
-    y_ret = [a if i==0 else a[1:] for i, a in enumerate(split(y, nan_pos))]
+    x_ret = [a if i==0 else a[1:] for i, a in enumerate(np.split(x, nan_pos))]
+    y_ret = [a if i==0 else a[1:] for i, a in enumerate(np.split(y, nan_pos))]
     return x_ret, y_ret
