947d6884c2c13756c533a87eb060e922e0bc15eb
Jan Kiene <jankiene@onlinehome.de>
2018-09-29 20:23:12 +0200

    function for merging mono wavfiles

--- audio/utils.py
+++ audio/utils.py
@@ -12,7 +12,17 @@ def extract_channels_from_wav(filename, channels, write_file=None):
     s, fs = sf.read(filename)
     if type(channels) == int:
         channels = [channels]
-    s = s[:,channels]
+    s = s[: ,channels]
     if write_file is not None:
         sf.write(write_file, s, fs)
     return s
+
+
+def monofiles_to_multitrack(monofiles, new_filename):
+    """Read mono wav files and combine them into a multitrack wavfile"""
+    signals = []
+    for f in monofiles:
+        s, fs = sf.read(f)
+        signals.append(s)
+    multitrack_array = np.asarray(signals).T
+    sf.write(new_filename, multitrack_array, fs)
